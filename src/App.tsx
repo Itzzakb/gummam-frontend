31dee332c839f8b865b9a85f7b1b941f8588c3aa
Itzzakb <103726648+Itzzakb@users.noreply.github.com>
2026-05-20 10:18:04 +0530

    scroll to top

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { LandingPage } from './pages/LandingPage';
 import { PageLayout } from './components/layout/PageLayout';
 import { ListingProjects } from './pages/ListingProjects';
+import { ScrollToTop } from './components/layout/ScrollToTop';
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<PageLayout />}>
           <Route index element={<LandingPage />} />
